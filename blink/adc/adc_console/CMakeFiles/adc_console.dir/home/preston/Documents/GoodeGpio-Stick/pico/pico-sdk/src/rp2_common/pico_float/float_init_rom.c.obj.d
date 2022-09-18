3d2bc0cf395f791ce87bf3531b4e997d3a50bf47
pgoode41 <pgoode41@gmail.com>
2022-09-18 00:18:44 -0500

    oops

---
+++ blink/adc/adc_console/CMakeFiles/adc_console.dir/home/preston/Documents/GoodeGpio-Stick/pico/pico-sdk/src/rp2_common/pico_float/float_init_rom.c.obj.d
@@ -0,0 +1,31 @@
+adc/adc_console/CMakeFiles/adc_console.dir/home/preston/Documents/GoodeGpio-Stick/pico/pico-sdk/src/rp2_common/pico_float/float_init_rom.c.obj: \
+ /home/preston/Documents/GoodeGpio-Stick/pico/pico-sdk/src/rp2_common/pico_float/float_init_rom.c \
+ /usr/include/newlib/string.h /usr/include/newlib/_ansi.h \
+ /usr/include/newlib/newlib.h /usr/include/newlib/_newlib_version.h \
+ /usr/include/newlib/sys/config.h /usr/include/newlib/machine/ieeefp.h \
+ /usr/include/newlib/sys/features.h /usr/include/newlib/sys/reent.h \
+ /usr/include/newlib/_ansi.h \
+ /usr/lib/gcc/arm-none-eabi/6.3.1/include/stddef.h \
+ /usr/include/newlib/sys/_types.h /usr/include/newlib/machine/_types.h \
+ /usr/include/newlib/machine/_default_types.h \
+ /usr/include/newlib/sys/lock.h /usr/include/newlib/sys/cdefs.h \
+ /usr/include/newlib/sys/string.h \
+ /home/preston/Documents/GoodeGpio-Stick/pico/pico-sdk/src/rp2_common/pico_bootrom/include/pico/bootrom.h \
+ /home/preston/Documents/GoodeGpio-Stick/pico/pico-sdk/src/common/pico_base/include/pico.h \
+ /home/preston/Documents/GoodeGpio-Stick/pico/pico-sdk/src/common/pico_base/include/pico/types.h \
+ /home/preston/Documents/GoodeGpio-Stick/pico/pico-sdk/src/common/pico_base/include/pico/assert.h \
+ /usr/lib/gcc/arm-none-eabi/6.3.1/include/stdbool.h \
+ /usr/include/newlib/assert.h \
+ /usr/lib/gcc/arm-none-eabi/6.3.1/include/stdint.h \
+ /usr/include/newlib/stdint.h /usr/include/newlib/sys/_intsup.h \
+ /usr/include/newlib/sys/_stdint.h \
+ /home/preston/Documents/pico-sdk/pico-examples/blink/generated/pico_base/pico/version.h \
+ /home/preston/Documents/GoodeGpio-Stick/pico/pico-sdk/src/common/pico_base/include/pico/config.h \
+ /home/preston/Documents/pico-sdk/pico-examples/blink/generated/pico_base/pico/config_autogen.h \
+ /home/preston/Documents/GoodeGpio-Stick/pico/pico-sdk/src/boards/include/boards/pico.h \
+ /home/preston/Documents/GoodeGpio-Stick/pico/pico-sdk/src/rp2_common/cmsis/include/cmsis/rename_exceptions.h \
+ /home/preston/Documents/GoodeGpio-Stick/pico/pico-sdk/src/rp2_common/pico_platform/include/pico/platform.h \
+ /home/preston/Documents/GoodeGpio-Stick/pico/pico-sdk/src/rp2040/hardware_regs/include/hardware/platform_defs.h \
+ /home/preston/Documents/GoodeGpio-Stick/pico/pico-sdk/src/common/pico_base/include/pico/types.h \
+ /home/preston/Documents/GoodeGpio-Stick/pico/pico-sdk/src/common/pico_base/include/pico/error.h \
+ /home/preston/Documents/GoodeGpio-Stick/pico/pico-sdk/src/rp2_common/pico_bootrom/include/pico/bootrom/sf_table.h
---
+++ blink/hello_world/serial/CMakeFiles/hello_serial.dir/home/preston/Documents/GoodeGpio-Stick/pico/pico-sdk/src/rp2_common/pico_standard_link/binary_info.c.obj.d
@@ -0,0 +1,31 @@
+hello_world/serial/CMakeFiles/hello_serial.dir/home/preston/Documents/GoodeGpio-Stick/pico/pico-sdk/src/rp2_common/pico_standard_link/binary_info.c.obj: \
+ /home/preston/Documents/GoodeGpio-Stick/pico/pico-sdk/src/rp2_common/pico_standard_link/binary_info.c \
+ /home/preston/Documents/GoodeGpio-Stick/pico/pico-sdk/src/common/pico_binary_info/include/pico/binary_info.h \
+ /home/preston/Documents/GoodeGpio-Stick/pico/pico-sdk/src/common/pico_binary_info/include/pico/binary_info/defs.h \
+ /home/preston/Documents/GoodeGpio-Stick/pico/pico-sdk/src/common/pico_binary_info/include/pico/binary_info/structure.h \
+ /usr/lib/gcc/arm-none-eabi/6.3.1/include/stdint.h \
+ /usr/include/newlib/stdint.h \
+ /usr/include/newlib/machine/_default_types.h \
+ /usr/include/newlib/sys/features.h /usr/include/newlib/_newlib_version.h \
+ /usr/include/newlib/sys/_intsup.h /usr/include/newlib/sys/_stdint.h \
+ /home/preston/Documents/GoodeGpio-Stick/pico/pico-sdk/src/common/pico_binary_info/include/pico/binary_info/code.h \
+ /home/preston/Documents/GoodeGpio-Stick/pico/pico-sdk/src/common/pico_base/include/pico.h \
+ /home/preston/Documents/GoodeGpio-Stick/pico/pico-sdk/src/common/pico_base/include/pico/types.h \
+ /home/preston/Documents/GoodeGpio-Stick/pico/pico-sdk/src/common/pico_base/include/pico/assert.h \
+ /usr/lib/gcc/arm-none-eabi/6.3.1/include/stdbool.h \
+ /usr/include/newlib/assert.h /usr/include/newlib/_ansi.h \
+ /usr/include/newlib/newlib.h /usr/include/newlib/sys/config.h \
+ /usr/include/newlib/machine/ieeefp.h \
+ /usr/lib/gcc/arm-none-eabi/6.3.1/include/stddef.h \
+ /home/preston/Documents/pico-sdk/pico-examples/blink/generated/pico_base/pico/version.h \
+ /home/preston/Documents/GoodeGpio-Stick/pico/pico-sdk/src/common/pico_base/include/pico/config.h \
+ /home/preston/Documents/pico-sdk/pico-examples/blink/generated/pico_base/pico/config_autogen.h \
+ /home/preston/Documents/GoodeGpio-Stick/pico/pico-sdk/src/boards/include/boards/pico.h \
+ /home/preston/Documents/GoodeGpio-Stick/pico/pico-sdk/src/rp2_common/cmsis/include/cmsis/rename_exceptions.h \
+ /home/preston/Documents/GoodeGpio-Stick/pico/pico-sdk/src/rp2_common/pico_platform/include/pico/platform.h \
+ /home/preston/Documents/GoodeGpio-Stick/pico/pico-sdk/src/rp2040/hardware_regs/include/hardware/platform_defs.h \
+ /usr/include/newlib/sys/cdefs.h \
+ /home/preston/Documents/GoodeGpio-Stick/pico/pico-sdk/src/common/pico_base/include/pico/types.h \
+ /home/preston/Documents/GoodeGpio-Stick/pico/pico-sdk/src/common/pico_base/include/pico/error.h \
+ /home/preston/Documents/GoodeGpio-Stick/pico/pico-sdk/src/rp2_common/boot_stage2/include/boot_stage2/config.h \
+ /home/preston/Documents/GoodeGpio-Stick/pico/pico-sdk/src/common/pico_base/include/pico/config.h
